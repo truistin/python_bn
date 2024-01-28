061dba2c034c5fcba69c727ac1a54a0705465a1a
truistin <truistin@gmail.com>
2024-01-28 20:47:51 +0800

    fix code

--- main/main.py
+++ main/main.py
@@ -4,11 +4,62 @@ from binance.lib.utils import config_logging
 from binance.websocket.cm_futures.websocket_client import CMFuturesWebsocketClient
 from binance.websocket.um_futures.websocket_client import UMFuturesWebsocketClient
 from binance.um_futures import UMFutures
+import json
+import schedule  
+import time  
+from apscheduler.schedulers.background import BackgroundScheduler  
+
 
 config_logging(logging, logging.DEBUG)
 
+class symbolInfo:
+    def __init__(self, symbol):
+        self.symbol = symbol
+    symbol = ""
+    mid_price = 0
+    sum_price = 0
+    time_stamp = 0 
+    thresh = 0
+    num = 0
+
+    def calc(ask, bid, stamp):
+        mid_price = (ask + bid) / 2
+        sum_price = sum_price + mid_price
+        num = num + 1
+        time_stamp = stamp
+
+
+eth_perp_sy = symbolInfo("ETHUSD_PERP")
+btc_perp_sy = symbolInfo("BTCUSD_PERP")
+sol_perp_sy = symbolInfo("SOLUSD_PERP")
+
+eth_swap_sy = symbolInfo("ETHUSDT")
+btc_swap_sy = symbolInfo("BTCUSDT")
+sol_swap_sy = symbolInfo("SOLUSDT")
+
+lst = []
+lst.append(eth_perp_sy)
+lst.append(btc_perp_sy)
+lst.append(sol_perp_sy)
+lst.append(eth_swap_sy)
+lst.append(btc_swap_sy)
+lst.append(sol_swap_sy)
+
 def message_handler(_, message):
-    print(message)
+    obj = json.loads(message)
+    for it in lst:
+        if it.symbol == obj.s:
+            it.calc(obj.a, obj.b, obj.T)
+    # print(message)
+
+def time_calc():
+    for it in lst:
+        it.thresh = it.sum_price / it.num
+        logging.info("time calc symbol : {}, thresh : {}, timestamp : {}, \
+                    mid_price : {}, ".format(it.symbol, it.thresh, it.timestamp, it.mid_price))
+        it.num = 0
+        it.sum_price = 0
+        
 
 def subscribeUM():
     my_client = UMFuturesWebsocketClient(on_message=message_handler)
@@ -40,3 +91,8 @@ def subscribeCM():
 if __name__ == "__main__":
     subscribeUM()
     subscribeCM()
+
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(time_calc, 'interval', seconds=300)
+    scheduler.start()
+    
