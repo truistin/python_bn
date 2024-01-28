b146ef78594aacbd53fb641c93acee224bde40e5
truistin <truistin@gmail.com>
2024-01-28 21:28:09 +0800

    fix code

--- main/main.py
+++ main/main.py
@@ -45,7 +45,7 @@ lst.append(btc_swap_sy)
 lst.append(sol_swap_sy)
 
 def message_handler(_, message):
-    # print(message)
+    print(message)
     obj = json.loads(message)
     for it in lst:
         if it.symbol == obj["s"]:
