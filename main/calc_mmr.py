a282efcced3f7dc867b69b5c29aa395dfb3755e6
taoaishan <m15317939076@163.com>
2024-01-29 22:42:59 +0800

    fix code

--- main/calc_mmr.py
+++ main/calc_mmr.py
@@ -17,9 +17,10 @@ class BinanceMR(object):
     def __init__(self,apikey,apisecret):
         self.origin_api_clt = PortfolioClient(api_key=apikey,api_secret=apisecret)
         self.request_clt = RequestClient(api_key=apikey,secret_key=apisecret, url='https://api.binance.com')
-        self.collateralRate = {}
+        self.collateralRate = {} #抵押品率
         self.spotData = {}
-        self.swapData = {}
+        self.uswapData = {}
+        self.cswapData = {}
         self.marginPositions = {}
         self.umPositions = {}
         self.cmPositions = {}
@@ -69,21 +70,29 @@ class BinanceMR(object):
             symbol = data['asset']
             self.collateralRate[symbol] = data
 
-    def get_um_brackets(self, symbol, leverage): ##获取U本位维持保证金率
+    def get_um_brackets(self, symbol, usdtVal): ##获取U本位维持保证金率
         params = {'symbol':symbol}
         response = self.origin_api_clt.futures_leverage_bracket(**params)
         for item in response[0]['brackets']:
-            if leverage == item['initialLeverage']:
-                return item
-        return None
+            notionalFloor = item['notionalFloor'] #持仓USDT最小值
+            notionalCap = item['notionalCap']   # 持仓USDT最大值
+            maintMarginRatio = item['maintMarginRatio'] #维持保证金率
+            cum = item ['cum'] #维持保证金速算额
+            if usdtVal >= notionalFloor and usdtVal <= notionalCap:
+                return Decimal(maintMarginRatio), Decimal(cum)
+        return None, None
 
-    def get_cm_brackets(self, symbol, leverage): #获取币本位维持保证金率
+    def get_cm_brackets(self, symbol, symbolVal): #获取币本位维持保证金率
         params = {'symbol':symbol}
         response = self.origin_api_clt.cmfu_leverage_bracket(**params)
         for item in response[0]['brackets']:
-            if leverage == item['initialLeverage']:
-                return item
-        return None
+            qtyFloor = item['qtyFloor'] #持仓币数量最小值
+            qtyCap = item['qtyCap']     #持仓币数量最大值
+            maintMarginRatio = item['maintMarginRatio'] #维持保证金比率
+            cum = item['cum'] #维持保证金速算额
+            if symbolVal >= qtyFloor and symbolVal <= qtyCap:
+                return Decimal(maintMarginRatio), Decimal(cum)
+        return None, None
 
 
     def get_uniMMR(self): ##获取uniMMR
@@ -97,17 +106,23 @@ class BinanceMR(object):
         response = requests.get("https://api.binance.com/api/v3/ticker/24hr")
         spotRet = response.json() if response.status_code == 200 else []
         response = requests.get("https://fapi.binance.com/fapi/v1/ticker/24hr")
-        swapRet = response.json() if response.status_code == 200 else []
+        uswapRet = response.json() if response.status_code == 200 else []
+        response = requests.get("https://dapi.binance.com/dapi/v1/ticker/24hr")
+        cswapRet = response.json() if response.status_code == 200 else []
         self.spotData = {i["symbol"]: i for i in spotRet}
-        self.swapData = {i["symbol"]: i for i in swapRet}
+        self.uswapData = {i["symbol"]: i for i in uswapRet}
+        self.cswapData = {i["symbol"]: i for i in cswapRet}
 
     def get_last_price(self, symbol): ##获取最新价格
-        if 'USDT' not in symbol:
+        if symbol == 'USDT':
+            return 1
+        elif 'USD_PERP' in symbol:
+            return float(self.cswapData[f"{symbol.upper()}"]["lastPrice"]) if f"{symbol.upper()}" in self.cswapData.keys() else np.nan
+        elif 'USDT' in symbol:
+            return float(self.uswapData[f"{symbol.upper()}"]["lastPrice"]) if f"{symbol.upper()}" in self.uswapData.keys() else np.nan
+        else:
             symbol = f'{symbol}USDT'
             return float(self.spotData[f"{symbol.upper()}"]["lastPrice"]) if f"{symbol.upper()}" in self.spotData.keys() else np.nan
-        else:
-            return float(self.swapData[f"{symbol.upper()}"]["lastPrice"]) if f"{symbol.upper()}" in self.spotData.keys() else np.nan
-
     def trade_loss(self): ##开仓亏损
         loss_sum = 0
         for symbol, val in self.marginPositions.items():
@@ -124,7 +139,7 @@ class BinanceMR(object):
             
     def calc_margin_equity_im_mm(self): #计算杠杆账户权益, im, mm
         margin_equity_im_mm = {}
-        margin_mm_sum = Decimal(0)
+        margin_equity_sum = Decimal(0)
         for symbol, val in self.marginPositions.items():
             equity_im_mm = {}
             leverage = self.margin_leverage[symbol] if symbol in self.margin_leverage.keys() else self.margin_leverage['default']
@@ -134,137 +149,60 @@ class BinanceMR(object):
             if symbol in ('USDT', 'USDC', 'BUSD'):
                 equity_im_mm['mm'] = Decimal(0)
             else:
-                equity_im_mm['mm'] = Decimal(val['crossMarginBorrowed']) * Decimal(self.margin_mmr[leverage])  * Decimal(self.get_priceIndex(f'{symbol}USDT')) #维持保证金
-                margin_mm_sum += equity_im_mm['mm']
+                index_price = self.get_last_price(symbol)
+                equity_im_mm['mm'] = Decimal(val['crossMarginBorrowed']) * Decimal(self.margin_mmr[leverage])  * Decimal(index_price) #维持保证金
             margin_equity_im_mm[symbol] = equity_im_mm
-        print('margin_mm_sum', margin_mm_sum)
+            margin_equity_sum += equity_im_mm['equity']
+        #print('margin_equity_sum', margin_equity_sum)
         return margin_equity_im_mm
 
     def calc_cswap_equity_im_mm(self): #计算币本位账户权益，im，mm
         cswap_equity_im_mm = {}
-        cswap_mm_sum = Decimal(0)
+        cswap_equity_sum = Decimal(0)
         for symbol, val in self.cmPositions.items():
             equity_im_mm = {}
             leverage = Decimal(val['leverage'])
             markPrice = Decimal(val['markPrice'])
             symbol_base = symbol.replace('USD_PERP','')
             if symbol == 'BTCUSD_PERP':
-                qty = Decimal(val['positionAmt']) * Decimal(100)
+                qty = Decimal(val['positionAmt']) * Decimal(100) / markPrice
             else:
-                qty = Decimal(val['positionAmt']) * Decimal(10)
-            item = self.get_cm_brackets(f'{symbol}', int(leverage))
-            mmr = Decimal(item['maintMarginRatio'])
+                qty = Decimal(val['positionAmt']) * Decimal(10) / markPrice
+            mmr, cum = self.get_cm_brackets(symbol, abs(qty))
             equity_im_mm['equity'] = Decimal(self.marginPositions[symbol_base]['cmWalletBalance']) + Decimal(self.marginPositions[symbol_base]['cmUnrealizedPNL'])
             equity_im_mm['im'] = abs(qty) * (Decimal(1)/leverage) / markPrice
-            equity_im_mm['mm'] = abs(qty) * mmr / Decimal(leverage)
-            cswap_mm_sum += equity_im_mm['mm']
+            equity_im_mm['mm'] = (abs(qty) * mmr -cum) * markPrice
+            cswap_equity_sum += equity_im_mm['equity']
             #print('aaaa', equity_im_mm['mm'])
             cswap_equity_im_mm[symbol] = equity_im_mm
-        print('cswap_mm_sum', cswap_mm_sum)
+        #print('cswap_equity_sum', cswap_equity_sum)
         return cswap_equity_im_mm
     
     def calc_uswap_equity_im_mm(self): #计算U本位账户权益，im，mm
         uswap_equity_im_mm = {}
-        uswap_mm_sum = Decimal(0)
+        uswap_equity_sum = Decimal(0)
         for symbol, val in self.umPositions.items():
             equity_im_mm = {}
             symbol_base = symbol.replace('USDT', '')
             leverage = Decimal(val['leverage'])
             markPrice = Decimal(val['markPrice'])
             qty = Decimal(val['positionAmt'])
-            item = self.get_um_brackets(symbol, int(leverage))
-            mmr = Decimal(item['maintMarginRatio'])
+            mmr, cum = self.get_um_brackets(symbol, abs(qty) * markPrice)
             equity_im_mm['equity'] = Decimal(self.marginPositions[symbol_base]['umWalletBalance']) + Decimal(self.marginPositions[symbol_base]['umUnrealizedPNL'])
             equity_im_mm['im'] = abs(qty) * markPrice * (Decimal(1)/leverage) #初始保证金
-            equity_im_mm['mm'] = abs(qty) * markPrice * mmr /Decimal(leverage)#维持保证金
-            print('ssssssss', symbol, equity_im_mm['mm'], qty, markPrice, mmr, leverage)
+            equity_im_mm['mm'] = abs(qty) * markPrice * mmr - cum#维持保证金
+            #print('ssssssss', symbol, equity_im_mm['mm'], qty, markPrice, mmr, leverage)
             uswap_equity_im_mm[symbol] = equity_im_mm
-            uswap_mm_sum += equity_im_mm['mm']
-        print('uswap_mm_sum', uswap_mm_sum)
+            uswap_equity_sum += equity_im_mm['equity']
+        #print('uswap_equity_sum', uswap_equity_sum)
         #print(self.marginPositions)
         return uswap_equity_im_mm
     
-    def calc_uniAccount_equity_im_mmr(self):
-        trade_loss = self.trade_loss()
-        margin_equity_im_mm = self.calc_margin_equity_im_mm()
-        uswap_equity_im_mm = self.calc_uswap_equity_im_mm()
-        cswap_equity_im_mm = self.calc_cswap_equity_im_mm()
-        
-        uniAccount_equity_sum = Decimal(0)
-        uniAccount_im_sum = Decimal(0)
-        uniAccount_mm_sum = Decimal(0)
-        for symbol, val in margin_equity_im_mm.items():
-            if symbol == 'USDT' or symbol == 'USDC' or symbol == 'BUSD':
-                index_price = Decimal(1)
-                uniAccount_equity_sum += val['equity'] * index_price
-            else:
-                index_price = Decimal(self.get_priceIndex(f'{symbol}USDT'))
-                uniAccount_equity_sum += min(val['equity'] * index_price * Decimal(self.collateralRate[symbol]['collateralRate']), val['equity'] * index_price)
-                uniAccount_mm_sum += Decimal(val['mm'])
-            
-        
-        for symbol, val in uswap_equity_im_mm.items():
-            index_price = Decimal(self.get_priceIndex(f'{symbol}'))
-            uniAccount_equity_sum += val['equity'] * index_price
-            uniAccount_mm_sum += Decimal(val['mm'])
-        
-        for symbol, val in cswap_equity_im_mm.items():
-            symbol_base = symbol.replace('USD_PERP', '')
-            index_price = Decimal(self.get_priceIndex(f'{symbol_base}USDT'))
-            uniAccount_equity_sum += min(val['equity'] * index_price * Decimal(self.collateralRate[symbol_base]['collateralRate']),val['equity'] * index_price)
-            uniAccount_mm_sum += Decimal(val['mm'])
-        uniAccount_equity_sum += trade_loss
-        print('uniAccount_equity_sum', uniAccount_equity_sum)
-        print('uniAccount_mm_sum', uniAccount_mm_sum)
-
-
-        '''
-        uniAccount_equity_im_mm = {}
-        
-        
-        
-        um_im_sum = Decimal(0)
-        um_mm_sum = Decimal(0)
-        
-
-
-        for symbol, val in uswap_equity_im_mm.items():
-            um_im_sum += val['im']
-            um_mm_sum += val['mm']
-        for symbol, val in self.marginPositions.items():
-            equity_im_mm = {}
-            if symbol == 'USDT':
-                equity_im_mm['index_price'] = Decimal(1)
-                equity_im_mm['collateral_rate'] = Decimal(1)
-                equity_im_mm['equity'] = Decimal(uswap_equity) + Decimal(margin_equity_im_mm[symbol]['equity'])
-                equity_im_mm['trade_loss'] = Decimal(trade_loss)
-                equity_im_mm['im'] = Decimal(margin_equity_im_mm[symbol]['im']) + um_im_sum
-                equity_im_mm['mm'] = Decimal(margin_equity_im_mm[symbol]['mm']) + um_mm_sum
-            else:
-                equity_im_mm['index_price'] = Decimal(self.get_priceIndex(f'{symbol}USDT'))
-                equity_im_mm['collateral_rate'] = Decimal(self.collateralRate[symbol]['collateralRate'])
-                if f'{symbol}USD_PERP' in cswap_equity_im_mm.keys():
-                    equity_im_mm['equity'] = Decimal(margin_equity_im_mm[symbol]['equity']) + Decimal(cswap_equity_im_mm[f'{symbol}USD_PERP']['equity'])
-                    equity_im_mm['im'] = margin_equity_im_mm[symbol]['im'] + Decimal(cswap_equity_im_mm[f'{symbol}USD_PERP']['im'])
-                    equity_im_mm['mm'] = margin_equity_im_mm[symbol]['mm'] + Decimal(cswap_equity_im_mm[f'{symbol}USD_PERP']['mm'])
-                else:
-                    equity_im_mm['equity'] = Decimal(margin_equity_im_mm[symbol]['equity'])
-                    equity_im_mm['im'] = margin_equity_im_mm[symbol]['im']
-                    equity_im_mm['mm'] = margin_equity_im_mm[symbol]['mm']
-                equity_im_mm['trade_loss'] = Decimal(0)
-            uniAccount_equity_im_mm[symbol] = equity_im_mm 
-        return uniAccount_equity_im_mm
-        '''
-    
     def calc_uniMMR(self):
-        uniAccount_equity_im_mm = self.calc_uniAccount_equity_im_mmr()
-        uniAccount_equity = Decimal(0)
-        uniAccount_mm = Decimal(0)
-        for key, val in uniAccount_equity_im_mm.items():
-            uniAccount_equity += val['equity'] * val['collateral_rate'] * val['index_price'] + val['trade_loss']
-            uniAccount_mm += val['mm'] * val['index_price']
+        uniAccount_equity = self.calc_equity()
+        uniAccount_mm = self.calc_mm()
         print(uniAccount_equity, uniAccount_mm)
-        return uniAccount_equity/uniAccount_mm
+        return Decimal(uniAccount_equity)/Decimal(uniAccount_mm)
     
     def get_account(self):
         params = {}
@@ -281,29 +219,71 @@ class BinanceMR(object):
         response = self.origin_api_clt._request_margin_api('get', 'um/account', True, data = params)
         return response
 
+    def calc_mm(self):
+        sum_mm = Decimal(0)
+        for symbol, val in self.marginPositions.items(): #杠杆现货mm
+            leverage = self.margin_leverage[symbol] if symbol in self.margin_leverage.keys() else self.margin_leverage['default']
+            if symbol in ('USDT', 'USDC', 'BUSD'):
+                mm = Decimal(0)
+            else:
+                price = self.get_last_price(symbol)
+                mm = Decimal(val['crossMarginBorrowed']) * Decimal(self.margin_mmr[leverage])  * Decimal(price) #维持保证金
+                sum_mm += mm
+
+        for symbol, val in self.umPositions.items(): #u本位mm
+            markPrice = Decimal(val['markPrice'])
+            qty = Decimal(val['positionAmt'])
+            mmr, cum = self.get_um_brackets(symbol, abs(qty) * markPrice)
+            mm = abs(qty) * markPrice * mmr - cum#维持保证金
+            sum_mm += mm
+
+        for symbol, val in self.cmPositions.items(): #币本位mm
+            markPrice = Decimal(val['markPrice'])
+            if symbol == 'BTCUSD_PERP':
+                qty = Decimal(val['positionAmt']) * Decimal(100) / markPrice
+            else:
+                qty = Decimal(val['positionAmt']) * Decimal(10) / markPrice
+            mmr, cum = self.get_cm_brackets(symbol, abs(qty))
+            mm = (abs(qty) * mmr -cum) * markPrice
+            sum_mm += mm
+        
+        return sum_mm
+            
+    def calc_equity(self):
+        sum_equity = Decimal(0)
+        for symbol, val in self.marginPositions.items():
+            if Decimal(val['crossMarginAsset']) != 0 or Decimal(val['crossMarginBorrowed']) != 0:
+                rate = Decimal(self.collateralRate[symbol]['collateralRate'])
+                equity = Decimal(val['crossMarginAsset']) - Decimal(val['crossMarginBorrowed'])
+                price = Decimal(self.get_last_price(symbol))
+                calc_equity = min(equity*price, equity*price*rate)
+                sum_equity += calc_equity
+                print('margin', val['asset'], '全仓资产:',val['crossMarginAsset'],'全仓杠杆借贷:',val['crossMarginBorrowed'], 'equity:', calc_equity)
+            if Decimal(val['umWalletBalance']) != 0 or Decimal(val['umUnrealizedPNL']) != 0:
+                rate = Decimal(self.collateralRate[symbol]['collateralRate'])
+                equity = Decimal(val['umWalletBalance']) +  Decimal(val['umUnrealizedPNL'])
+                price = Decimal(1)
+                calc_equity = equity * price
+                sum_equity += calc_equity
+                print('um', f"{val['asset']}USDT", 'U本位钱包余额:',val['umWalletBalance'],'U本位未实现盈亏:', val['umUnrealizedPNL'], 'equity:', calc_equity)
+            if Decimal(val['cmWalletBalance']) != 0 or Decimal(val['cmUnrealizedPNL']) != 0:
+                rate = Decimal(self.collateralRate[symbol]['collateralRate'])
+                equity = Decimal(val['cmWalletBalance']) + Decimal(val['cmUnrealizedPNL'])
+                price = Decimal(self.get_last_price(symbol))
+                calc_equity = min(equity*price, equity*price*rate)
+                sum_equity += calc_equity
+                print('cm', f"{val['asset']}USD_PERP", '币本位钱包余额:',val['cmWalletBalance'],'币本位未实现盈亏:', val['cmUnrealizedPNL'], 'equity:', calc_equity)
+        return sum_equity
+
+
 if __name__ == '__main__':
     #apikey = 'qrNt0VO8sdAbeLS0bAMbtDyip67ZwVUFU6XGVtlQw1anjeiyLOwfNrcVdqIAMyMR'
     #secretkey = '8aEKZ23CEBAECqDUG7XD5hJuDAUtsUe1f8uNoh0NOAMKL12ySTDfbV5hjQzJcqrG'
-    apikey = '3IepehoMkZWXPq7vnZmpPHVitb3YXrw35SCuIHjui1AFAuqJeF879Sw8hzbLDRXx'
-    secretkey = 'ssFbZD2aiBnN6INEGtreutHGdvNZIMxbj1SpyFvk2VdolwijcWZ9C5bpyfNuw1iX'
+    apikey = '7uky94bXR47G1ry1q7djuh6PVItKGdqKwXx4audvl5DzvR6XCMhJhCg1xECIJj7D'
+    secretkey = 'T8mFrsqJylHa2spAw4FN6p0HYOCPdnsBnIhrAf5BuT628s3rjR6yjPEUYW34IFve'
     binanceMr = BinanceMR(apikey,secretkey)
     binanceMr.initialize()
-    binanceMr.calc_uniAccount_equity_im_mmr()
-    #print('calc_uniMMR:',binanceMr.calc_uniMMR())
-    #print('uniMMR:',binanceMr.get_uniMMR())
+    print(binanceMr.calc_uniMMR()) # 计算uniMMR
+    print(binanceMr.get_uniMMR()) # 接口获取uniMMR
     
-    print('111',binanceMr.get_account())
-    #print(binanceMr.marginPositions['BTC'])
-    
-    resp = binanceMr.get_um_account()
-    for item in resp['assets']:
-        print(item['asset'], item['maintMargin'])
-    for item in resp['positions']:
-        print(item['symbol'], item['maintMargin'], item['positionAmt'], item['leverage'])
-    print('1111111')
-    resp = binanceMr.get_cm_account()
-    for item in resp['assets']:
-        print(item['asset'], item['maintMargin'])
-    
-    #print(binanceMr.marginPositions)
-    
+   
